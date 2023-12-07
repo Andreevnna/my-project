77766af57a0ad234afd9fee85041d69f8bf7de22
Andreevnna <101506097+Andreevnna@users.noreply.github.com>
2023-12-07 20:50:34 +0300

    Revert "Coffee House"

--- coffee-house/index.js
+++
@@ -1,7 +0,0 @@
-// let toogle = document.querySelector('.toogle')
-
-// let event = document.addEventListener('click', function(e){
-//     if(e.target === toogle){
-//         console.log(e.target)
-//     }
-// })
