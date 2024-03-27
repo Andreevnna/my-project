94b2ff6dce99180cf085cc88b8c53f853ae9eef7
andreevna <anastasija262@yandex.ru>
2024-03-27 20:43:57 +0300

    feat: add slider

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -8,50 +8,90 @@ const toggleMobileMenu = () => {
 };
 
 document.addEventListener('click', (event) => {
-  if (event.target === burger || event.target.classList.contains('button-menu-mobile__line')) {
+  if (event.target === burger) {
     toggleMobileMenu();
   } else if (menuMobile.classList.contains('open') && event.target !== menuMobile) {
     toggleMobileMenu();
   }
 });
 
-// slider variables 
-const sliderCardsArr = document.querySelectorAll('.slider__block');
-const sliderFillArr = document.querySelectorAll('.pagination__label_fill');
-const leftSliderBtn = document.getElementById('leftBtn');
-const rightSliderBtn = document.getElementById('rightBtn');
+// const slider = document.querySelector(".slider");
+// const slides = document.querySelectorAll(".slider__block");
+// const indicators = document.querySelectorAll(".slider__progress");
+// let currentSlide = 0;
 
-let slidePosition = 0;
-let sliderWidth = 0;
-let paginationIndex = 0;
+// function showSlide(n) {
+//     slides.forEach(slide => slide.style.display = "none");
+//     indicators.forEach(indicator => indicator.classList.remove("active"));
+    
+//     slides[n].style.display = "block";
+//     indicators[n].classList.add("active");
 
-// get width slider
-function getSliderWidth() {
-  sliderWidth = sliderCardsArr[0].offsetWidth;
-  return sliderWidth;
-};
+// }
 
-// change slider 
-function changeSlide(index) {
-  slidePosition = (-getSliderWidth()) * (index);
-  sliderCardsArr.forEach((cards) => {
-    cards.style.left = slidePosition + 'px';
-    });
-  paginationIndex = index;
+
+const arrSliders = document.querySelectorAll('.slider__block')
+const indicators = document.querySelectorAll('.slider__progress')
+
+let indexSlider = 0;
+let width = 0
+
+
+function sliderChange(index) {
+  arrSliders.forEach((cards) => {
+    cards.style.left = (-arrSliders[0].offsetWidth * index) + 'px'
+  })
+  // indicators.forEach(indicator => indicator.classList.remove("active"));
+  // indicators[index].classList.add("active");
 }
-// autoplay slider
-const commonTimer = setInterval(function changeSlideAtTime() {
-  paginationIndex = paginationIndex >= 2 ? 0 : paginationIndex + 1;
-  changeSlide(paginationIndex);
-}, 5500);
+function nextSlide() {
+  indexSlider = (indexSlider + 1) % arrSliders.length;
+  progressBar(indexSlider)
+  sliderChange(indexSlider);
+}
+function prevSlide() {
+  indexSlider = (indexSlider - 1 + arrSliders.length) % arrSliders.length;
+  sliderChange(indexSlider);
+}
+function autoSlide() {
+  nextSlide();
+}
 
-// buttons slider
-leftSliderBtn.addEventListener('click', () => {
-  paginationIndex = paginationIndex <= 0 ? 2 : paginationIndex - 1;
-  changeSlide(paginationIndex);
+let interval = setInterval(nextSlide, 5500);
+
+document.querySelector('.prev').addEventListener('click', () => {
+  clearInterval(interval)
+  prevSlide()
+  interval = setInterval(nextSlide, 4000);
+})
+document.querySelector('.next').addEventListener('click', () => {
+  clearInterval(interval)
+  nextSlide()
+  interval = setInterval(nextSlide, 4000);
+})
+
+function progressBar(index) {
+  let newInterval = setInterval(() => {
+    if (width <= 40) {
+      width++
+      indicators[index].style.width = width + 'px';
+    } else {
+      clearInterval(newInterval)
+    }
+  }, 100)
+}
+// tag click
+
+const menuTags = document.querySelectorAll('.button-tag');
+menuTags.forEach(tag => {
+    tag.addEventListener('click', function () {
+      menuTags.forEach(tag => tag.classList.remove('button_checked'));
+      this.classList.add('button_checked');
+    })
+})
+
+const myJson = await import("./data/products.json", {
+  with: { type: "json" },
 });
 
-rightSliderBtn.addEventListener('click', () => {
-  paginationIndex = paginationIndex >= 2 ? 0 : paginationIndex + 1;
-  changeSlide(paginationIndex);
-});
+// console.log(myJson.default)
